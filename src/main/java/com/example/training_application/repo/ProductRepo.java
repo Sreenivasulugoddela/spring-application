1e96ec57805a1c033fddbe12e11832004a97dc80
Sreenivasulu62 <goddela.sreenivasulu62@gmail.com>
2024-09-18 18:44:46 +0530

    new changes

--- src/main/java/com/example/training_application/repo/ProductRepo.java
+++ src/main/java/com/example/training_application/repo/ProductRepo.java
@@ -1,10 +1,10 @@
-package com.example.training_application.repo;
-
-import com.example.training_application.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
+//package com.example.training_application.repo;
+//
+//import com.example.training_application.model.Product;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProductRepo extends JpaRepository<Product, Long> {
+//
+//}
